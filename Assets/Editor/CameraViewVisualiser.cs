74c1a9d1dd2df6d69441b408fb2819bd0307c4bf
Ajay Gupta <relaxgameing8@gmail.com>
2026-08-05 16:35:40 +0530

    feat: basic editor and scene camera setup is done

---
+++ Assets/Editor/CameraViewVisualiser.cs
@@ -0,0 +1,91 @@
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class CameraViewVisualiser : EditorWindow {
+    private bool _isEnabled = true;
+    private Shader _shader;
+    private Material _mat;
+    private Camera _gameCam;
+
+    private void OnSceneCameraPostRender(ScriptableRenderContext scriptableRenderContext, Camera camera) {
+        if (!_isEnabled || camera.cameraType != CameraType.SceneView) {
+            return;
+        }
+
+        if (_gameCam == null) {
+            Debug.LogError("camera whose volume to visualise is not selected");
+            return;
+        }
+
+        if (_shader == null) {
+            _shader = Shader.Find("Learning/CameraVolumeVisualiser");
+        }
+
+        if (_shader  != null && _mat == null) {
+            _mat = new Material(_shader);
+        }
+
+        if (_mat != null)
+        {
+            RenderTexture activeRT = RenderTexture.active;
+            if (activeRT == null) {
+                return;
+            }
+            RenderTexture tempRT = RenderTexture.GetTemporary(
+                activeRT.width,
+                activeRT.height,
+                activeRT.depth,
+                activeRT.format
+            );
+
+            // 2. Copy current scene render into the temporary texture
+            Graphics.Blit(activeRT, tempRT);
+
+            // 3. Blit from temporary texture back to scene target through your material
+            Graphics.Blit(tempRT, activeRT, _mat);
+
+            // 4. Release the temporary texture back to Unity memory pool
+            RenderTexture.ReleaseTemporary(tempRT);
+            Debug.Log("camera view visualiser working");
+        }
+    }
+
+    [MenuItem("Tools/Camear view visualiser")]
+    private static void ShowWindow() {
+        var window = GetWindow<CameraViewVisualiser>();
+        window.titleContent = new GUIContent("Camera View Visualiser");
+        window.Show();
+    }
+
+    private void OnGUI() {
+        _shader = (Shader)EditorGUILayout.ObjectField(
+            "Effect Shader",
+            _shader,
+            typeof(Shader),
+            true
+        );
+
+        _gameCam = (Camera)EditorGUILayout.ObjectField(
+            "Game Camera",
+            _gameCam,
+            typeof(Camera),
+            true
+        );
+
+        if (GUILayout.Button($"{(_isEnabled ? "OFF" : "ON")} Visualiser",
+                GUILayout
+                    .Height(30))) {
+
+            _isEnabled = !_isEnabled;
+
+            if (_isEnabled) {
+                RenderPipelineManager.endCameraRendering += OnSceneCameraPostRender;
+            }
+            else {
+                RenderPipelineManager.endCameraRendering -= OnSceneCameraPostRender;
+            }
+        }
+    }
+
+}
